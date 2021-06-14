9714f0338b956a3416cd931d2180f9759ff70b5c
Aditya <aditya.khanal77@gmail.com>
2021-06-14 22:06:04 +0545

    BannerCompleted

---
+++ src/Components/HomeScreen/Row/Row.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Row.css';
+
+const Row = () => {
+    return (
+        <div className = "row">
+            <h1>I am a row</h1>
+            
+        </div>
+    )
+}
+
+export default Row
