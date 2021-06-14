ef5ddd9f72f149eb01cbd8f37e6c40d82d815804
Aditya <aditya.khanal77@gmail.com>
2021-06-14 10:58:43 +0545

    NavbarCompleted

---
+++ src/Components/HomeScreen/Nav/Nav.js
@@ -0,0 +1,37 @@
+import React,{useState, useEffect} from 'react';
+import './Nav.css';
+
+const Nav = () => {
+
+    const [show, handleShow] = useState(false);
+
+    const transitionNavbar = () => {
+        if(window.scrollY > 100){
+            handleShow(true);
+
+        }
+        else{
+            handleShow(false);
+        }
+    }
+
+
+    useEffect(() => {
+        window.addEventListener('scroll', transitionNavbar)
+
+
+        return () => window.removeEventListener('scroll',transitionNavbar)
+    },[])
+    return (
+        <div className = {`nav ${show && 'nav__black'}`}>
+            <div className="nav__contents">
+           <img className = "nav__logo" src="https://i1.wp.com/freepngimages.com/wp-content/uploads/2016/10/netflix-logo.png?fit=895%2C559g" alt="Image Not Found" />
+           <img className = "nav__avatar" src = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt = "Image Not Found" />
+           </div>
+          
+        </div>
+
+    )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAbeAqwEd_n_TTyoo9G-yH8GI_pCveJRvg",
+    authDomain: "netflix-clone-d54f3.firebaseapp.com",
+    projectId: "netflix-clone-d54f3",
+    storageBucket: "netflix-clone-d54f3.appspot.com",
+    messagingSenderId: "693547200166",
+    appId: "1:693547200166:web:4efb14deb0cfc3cb1b4e67"
+  };
